3364228482de6df66d283362b0baefb0bd379b64
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-12-02 11:38:18 -0300

    edt controller class, del ManyToOne

--- src/main/java/com/destinocerto/controller/DestinoController.java
+++ src/main/java/com/destinocerto/controller/DestinoController.java
@@ -3,6 +3,7 @@ package com.destinocerto.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.destinocerto.model.Destino;
 import com.destinocerto.repository.DestinoRepository;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 
 @RestController
 @RequestMapping("destinos")
